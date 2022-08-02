9838655313fe55deb1e1add247f627a4fd731b3d
michioxd <80969068+michioxd@users.noreply.github.com>
2022-08-02 13:13:41 +0700

    Merge branch 'master' of https://github.com/quancaiwin/quancaiwin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,9 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'quancaiwin', // Usually your GitHub org/user name.
-  projectName: 'quancaiwin', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  organizationName: 'quancaiwin',
+  projectName: 'quancaiwin',
+  deploymentBranch: 'gh-pages',
   i18n: {
     defaultLocale: 'vi',
     locales: ['vi'],
