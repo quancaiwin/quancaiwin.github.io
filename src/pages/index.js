c451d028b17910846583df15f4b17b542d0cdf9f
michioxd <80969068+michioxd@users.noreply.github.com>
2022-08-02 18:53:41 +0700

    set title to "home"

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Trang chủ`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
